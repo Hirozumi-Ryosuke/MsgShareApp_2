275b31b2d89a782bd1980d61e8e7ca139ba3f014
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-03-13 04:02:15 +0900

    Swimming only

--- app/src/main/java/com/example/msgshareapp_2/HobbiesActiviy.kt
+++ app/src/main/java/com/example/msgshareapp_2/HobbiesActiviy.kt
@@ -2,12 +2,21 @@ package com.example.msgshareapp_2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_hobbies.*
 
 class HobbiesActiviy : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hobbies)
+
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        val adapter = HobbiesAdapter(this, Suplier.hobbies)
+        recyclerView.adapter = adapter
     }
 
 }
--- app/src/main/java/com/example/msgshareapp_2/Model.kt
+++ app/src/main/java/com/example/msgshareapp_2/Model.kt
@@ -1,2 +1,46 @@
 package com.example.msgshareapp_2
 
+data class Hobby(var title: String)
+
+object Suplier {
+
+    val hobbies = listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking")
+    )
+
+
+}
